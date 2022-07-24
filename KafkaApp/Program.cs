d80f366fad148d97e8d41a3cc732955326e6f271
Abhishek Joshi <abhishek2185@gmail.com>
2022-07-24 13:57:00 +0530

    client kafka dotnet

---
+++ KafkaApp/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Confluent.Kafka;
+namespace KafkaApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var mode = "subscribe";
+            var brokerList = "localhost:19093";
+            var topics = new List<string>{
+                "myTopic"
+            };
+
+            Console.WriteLine($"Started consumer, Ctrl-C to stop consuming");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+
+            switch (mode)
+            {
+                case "subscribe":
+                    Run_Consume(brokerList, topics, cts.Token);
+                    break;
+                case "manual":
+                    Run_ManualAssign(brokerList, topics, cts.Token);
+                    break;
+                default:
+                    PrintUsage();
+                    break;
+            }
+        }
+        public static void Run_Consume(string brokerList, List<string> topics, CancellationToken cancellationToken)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:19092",
+                GroupId = "localhost",
+                SecurityProtocol = SecurityProtocol.Plaintext
+            };
+
+            var config_ssl = new ConsumerConfig
+            {
+                GroupId = "localhost",
+                BootstrapServers = "localhost:19093",
+                SecurityProtocol = SecurityProtocol.Ssl,
+                SslCaLocation = @"C:\source\git\kafka-docker\ssl\secrets\root-ca.crt",
+                SslCertificateLocation = @"C:\source\git\kafka-docker\ssl\secrets\kafka_client.crt",
+                SslKeyLocation = @"C:\source\git\kafka-docker\ssl\secrets\kafka_client.key",
+                SslEndpointIdentificationAlgorithm = SslEndpointIdentificationAlgorithm.None,
+                Debug = "Security"
+            };
+
+
+            // Note: If a key or value deserializer is not set (as is the case below), the 
+            // deserializer corresponding to the appropriate type from Confluent.Kafka.Deserializers
+            // will be used automatically (where available). The default deserializer for string
+            // is UTF8. The default deserializer for Ignore returns null for all input data
+            // (including non-null data).
+            using var consumer = new ConsumerBuilder<Null, string>(config_ssl).Build();
+            consumer.Subscribe(topics);
+
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var consumeResult = consumer.Consume(cancellationToken);
+
+                        if (consumeResult.IsPartitionEOF)
+                        {
+                            Console.WriteLine(
+                                $"Reached end of topic {consumeResult.Topic}, partition {consumeResult.Partition}, offset {consumeResult.Offset}.");
+
+                            continue;
+                        }
+
+                        Console.WriteLine($"Received message at {consumeResult.TopicPartitionOffset}: {consumeResult.Message.Value}");
+
+                        //if (consumeResult.Offset % commitPeriod == 0)
+                        //{
+                        // The Commit method sends a "commit offsets" request to the Kafka
+                        // cluster and synchronously waits for the response. This is very
+                        // slow compared to the rate at which the consumer is capable of
+                        // consuming messages. A high performance application will typically
+                        // commit offsets relatively infrequently and be designed handle
+                        // duplicate messages in the event of failure.
+                        try
+                        {
+                            consumer.Commit(consumeResult);
+                        }
+                        catch (KafkaException e)
+                        {
+                            Console.WriteLine($"Commit error: {e.Error.Reason}");
+                        }
+                        //}
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Consume error: {e.Error.Reason}");
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Closing consumer.");
+                consumer.Close();
+            }
+        }
+
+        public static void Producer()
+        {
+            var conf = new ProducerConfig { BootstrapServers = "localhost:19093" };
+
+            Action<DeliveryReport<Null, string>> handler = r =>
+                Console.WriteLine(!r.Error.IsError
+                    ? $"Delivered message to {r.TopicPartitionOffset}"
+                    : $"Delivery Error: {r.Error.Reason}");
+
+            using (var p = new ProducerBuilder<Null, string>(conf).Build())
+            {
+
+                for (int i = 0; i < 100; ++i)
+                {
+                    p.Produce("my-topic", new Message<Null, string> { Value = i.ToString() }, handler);
+                }
+
+                // wait for up to 10 seconds for any inflight messages to be delivered.
+                p.Flush(TimeSpan.FromSeconds(10));
+            }
+        }
+
+        static void ListGroups(string bootstrapServers)
+        {
+            using (var adminClient = new AdminClientBuilder(new AdminClientConfig { BootstrapServers = bootstrapServers }).Build())
+            {
+                // Warning: The API for this functionality is subject to change.
+                var topics = adminClient.GetMetadata(TimeSpan.FromSeconds(10));
+                var groups = adminClient.ListGroups(TimeSpan.FromSeconds(10));
+                Console.WriteLine($"Consumer Groups:");
+                foreach (var g in groups)
+                {
+                    Console.WriteLine($"  Group: {g.Group} {g.Error} {g.State}");
+                    Console.WriteLine($"  Broker: {g.Broker.BrokerId} {g.Broker.Host}:{g.Broker.Port}");
+                    Console.WriteLine($"  Protocol: {g.ProtocolType} {g.Protocol}");
+                    Console.WriteLine($"  Members:");
+                    foreach (var m in g.Members)
+                    {
+                        Console.WriteLine($"    {m.MemberId} {m.ClientId} {m.ClientHost}");
+                        Console.WriteLine($"    Metadata: {m.MemberMetadata.Length} bytes");
+                        Console.WriteLine($"    Assignment: {m.MemberAssignment.Length} bytes");
+                    }
+                }
+            }
+        }
+        /// <summary>
+        ///     In this example
+        ///         - consumer group functionality (i.e. .Subscribe + offset commits) is not used.
+        ///         - the consumer is manually assigned to a partition and always starts consumption
+        ///           from a specific offset (0).
+        /// </summary>
+        public static void Run_ManualAssign(string brokerList, List<string> topics, CancellationToken cancellationToken)
+        {
+            var config = new ConsumerConfig
+            {
+                // the group.id property must be specified when creating a consumer, even 
+                // if you do not intend to use any consumer group functionality.
+                GroupId = new Guid().ToString(),
+                BootstrapServers = brokerList,
+                // partition offsets can be committed to a group even by consumers not
+                // subscribed to the group. in this example, auto commit is disabled
+                // to prevent this from occurring.
+                EnableAutoCommit = true
+            };
+
+            using (var consumer =
+                new ConsumerBuilder<Ignore, string>(config)
+                    .SetErrorHandler((_, e) => Console.WriteLine($"Error: {e.Reason}"))
+                    .Build())
+            {
+                consumer.Assign(topics.Select(topic => new TopicPartitionOffset(topic, 0, Offset.Beginning)).ToList());
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var consumeResult = consumer.Consume(cancellationToken);
+                            // Note: End of partition notification has not been enabled, so
+                            // it is guaranteed that the ConsumeResult instance corresponds
+                            // to a Message, and not a PartitionEOF event.
+                            Console.WriteLine($"Received message at {consumeResult.TopicPartitionOffset}: ${consumeResult.Message.Value}");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Consume error: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    Console.WriteLine("Closing consumer.");
+                    consumer.Close();
+                }
+            }
+        }
+
+        private static void PrintUsage()
+            => Console.WriteLine("Usage: .. <subscribe|manual> <broker,broker,..> <topic> [topic..]");
+
+        
+    }
+}
